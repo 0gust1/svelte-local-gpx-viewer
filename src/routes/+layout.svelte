7a27e42a671077c9486395aa3dbe1f72ab3ee0fb
Augustin Chassine <augustin.chassine@mabsilico.com>
2024-11-12 12:25:47 +0100

    showcase: add github link and logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,9 +5,41 @@
 	let { children } = $props();
 </script>
 
-<h1 class="text-2xl font-light text-stone-600 mb-6 pt-3 pl-2 pb-1 bg-gradient-to-r from-slate-50 to-stone-300  shadow border-b border-b-stone-100/30 shadow-slate-500/30">Svelte local gpx viewer</h1>
-<div class="antialiased px-2">
+<header class="site-header">
+	<h1 class="title">Svelte local gpx viewer</h1>
+	<a class="github-link" href="https://github.com/0gust1/svelte-local-gpx-viewer">
+		<svg viewBox="0 0 98 96" xmlns="http://www.w3.org/2000/svg"
+			><path
+				fill-rule="evenodd"
+				clip-rule="evenodd"
+				d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"
+			/></svg
+		>
+		<span class="hidden"> Source </span>
+	</a>
+</header>
+<div class="px-2 antialiased">
 	<ParaglideJS {i18n}>
 		{@render children()}
 	</ParaglideJS>
 </div>
+
+<style lang="postcss">
+	.site-header {
+		@apply mb-6 border-b border-b-stone-100/30 bg-gradient-to-r from-slate-50 to-stone-300 pb-1 pl-2 pt-3 shadow shadow-slate-500/30;
+		@apply flex items-center;
+	}
+	.title {
+		@apply text-2xl font-light text-stone-600;
+	}
+	.github-link {
+		@apply ml-auto flex items-center justify-center fill-stone-500 pr-2;
+	}
+	.github-link:hover {
+		@apply fill-stone-600;
+	}
+	svg {
+		@apply block h-10 w-10;
+		@apply group-hover:fill-indigo-100;
+	}
+</style>
