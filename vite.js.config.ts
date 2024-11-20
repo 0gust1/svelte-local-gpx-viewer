554c5779fb35e3fca4c709ce8ba6cc8f7c6e21a3
Augustin Chassine <augustin.chassine@mabsilico.com>
2024-11-20 14:28:13 +0100

    feat: update vanilla js vite config and npm scripts (wip)

--- vite.js.config.ts
+++ vite.js.config.ts
@@ -1,3 +1,5 @@
+//https://stackoverflow.com/questions/75832641/how-to-compile-svelte-3-components-into-iifes-that-can-be-used-in-vanilla-js
+
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
@@ -6,8 +8,8 @@ export default defineConfig({
     build: {
         lib: {
             entry: resolve(__dirname, 'dist/index.js'),
-            name: 'Components',
-            fileName: 'components',
+            name: 'LocalGPXViewer',
+            fileName: 'local-gpx-viewer',
         },
         outDir: 'dist-js',
     },
