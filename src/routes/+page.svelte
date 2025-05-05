06b39ad8478ddd8455021a9f0058c354e94c5727
Augustin Chassine <augustin.chassine@mabsilico.com>
2025-05-05 13:55:01 +0200

    refactor: main page / routing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,11 @@
 <script lang="ts">
 	import { page } from '$app/state';
-	import { base } from '$app/paths';
 	import { blur, crossfade, draw, fade, fly, scale, slide } from 'svelte/transition';
-	import GpxLoad from '$lib/GPXLoad.svelte';
-	import LocalRoutesList from '$lib/LocalRoutesList.svelte';
-	import MapLibreWrapper from '$lib/MapLibreWrapper.svelte';
-	import RouteEdit from '$lib/route_edit/RouteEdit.svelte';
-	import { getUIRoutesManager } from '$lib/db_data/routesData.svelte';
+
 	import { stylesList, defaultStyle } from '$lib/maplibreStyles';
 
-	let uiRoutes = getUIRoutesManager();
+	import AllRoutes from './AllRoutes.svelte';
+	import Route from './Route.svelte';
 
 	let selectedStyle = $state(defaultStyle);
 	let pitch = $state(0);
@@ -28,75 +24,16 @@
 	let routeIdToEdit = $derived.by(() => {
 		return urlSearchParams?.get('id');
 	});
-
-	function round(value: number, precision: number) {
-		const multiplier = Math.pow(10, precision || 0);
-		return Math.round(value * multiplier) / multiplier;
-	}
 </script>
 
 <!-- url search: {@debug urlSearchParams} -->
 
 {#if shouldEdit && routeIdToEdit}
-	<a href="{base}/" class=" text-lg font-semibold text-blue-600">&lsaquo; all routes</a>
-{:else}
-	<h2 class="mb-4 text-2xl font-bold">Geo Routes</h2>
-{/if}
-{#if !shouldEdit || !routeIdToEdit}
-	<div class="flex w-full flex-col gap-3 sm:flex-row">
-		<div class="py-4">
-			<GpxLoad />
-		</div>
-		<div class="grow">
-			<LocalRoutesList />
-		</div>
-	</div>
-{/if}
-
-<div class="mx-auto mt-3">
-	{#if shouldEdit && routeIdToEdit}
-		<div class="relative" transition:fade>
-			<RouteEdit routeId={routeIdToEdit} mapStyle={selectedStyle} {pitch} />
-		</div>
-	{:else}
-		<div
-			class="grid items-center gap-1 border bg-slate-200 px-2 py-1 align-middle text-xs sm:grid-flow-col"
-		>
-			<label for="map-style" class="mr-2 text-right">Select Map Style:</label>
-			<select id="map-style" bind:value={selectedStyle} class="p-1 text-sm">
-				{#each stylesList as style}
-					<option value={style.style}>{style.name}</option>
-				{/each}
-			</select>
-
-			<label for="map-pitch" class="mr-2 text-right">Map perspective (pitch): {pitch}</label>
-			<input id="map-pitch" type="range" min="0" max="60" step="1" bind:value={pitch} />
-		</div>
-		<div class="relative">
-			{#if uiRoutes.selectedRoutesIds.size > 0}
-				<div
-					class="absolute top-0 right-0 z-50 flex flex-col rounded-bl-md bg-blue-100 p-2 text-xs shadow-sm"
-				>
-					<div class="flex flex-col items-end">
-						<span>selected routes: {uiRoutes.selectedRoutesIds.size}</span>
-						<span class="font-semibold">
-							{round(uiRoutes.selectedRoutesInfo.distance, 1)}km (+{round(
-								uiRoutes.selectedRoutesInfo.elevation.positive,
-								0
-							)}m, {round(uiRoutes.selectedRoutesInfo.elevation.negative, 0)}m)
-						</span>
-					</div>
-					<button
-						class="rounded-bl-md bg-blue-500 p-1 text-white"
-						onclick={() => {
-							uiRoutes.selectedRoutesIds.clear();
-						}}
-					>
-						Clear selection
-					</button>
-				</div>
-			{/if}
-			<MapLibreWrapper mapStyle={selectedStyle} {pitch} />
-		</div>
-	{/if}
+<div transition:fade={{ duration: 50 }}>
+	<Route routeIdToEdit={routeIdToEdit} selectedStyle={selectedStyle} pitch={pitch} />
 </div>
+{:else}
+<div transition:fade={{ duration: 50 }}>
+	<AllRoutes selectedStyle={selectedStyle} pitch={pitch} />
+</div>
+{/if}
---
+++ src/routes/Route.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import type { StyleSpecification } from 'maplibre-gl';
+	import { base } from '$app/paths';
+	import RouteEdit from '$lib/route_edit/RouteEdit.svelte';
+	interface Props {
+		routeIdToEdit: string;
+		selectedStyle: StyleSpecification;
+		pitch: number;
+	}
+	let { routeIdToEdit, selectedStyle, pitch }: Props = $props();
+</script>
+
+<a href="{base}/" class=" text-lg font-semibold text-blue-600">&lsaquo; all routes</a>
+
+<div class="mx-auto mt-3">
+	<div class="relative">
+		<RouteEdit routeId={routeIdToEdit} mapStyle={selectedStyle} {pitch} />
+	</div>
+</div>
