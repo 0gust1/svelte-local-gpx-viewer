ca23310c33c82e8f6433e412c9563ba1f036bfa9
Augustin Chassine <augustin.chassine@mabsilico.com>
2025-06-02 18:40:52 +0200

    feat: CE build command and test/dummy component

---
+++ src/lib/widgets/SimpleRoute.svelte
@@ -0,0 +1,54 @@
+<svelte:options customElement="gpx-simple-route" />
+
+<script lang="ts">
+  import type { Route, RouteInteractivePoint } from '$lib/db_data/routes.datatypes';
+  import { MapLibre, type StyleSpecification } from 'svelte-maplibre';
+  import MapLibreRoute from '$lib/MapLibreRoute.svelte';
+
+  interface Props {
+		route: Route;
+		mapStyle: StyleSpecification;
+		pitch: number;
+		photoSelection: { hovered: number | null; selected: number | null };
+		routePoint?: RouteInteractivePoint | null; // Add this property as optional
+	}
+  let { route: routeState, mapStyle, pitch, photoSelection, routePoint = null }: Props = $props();
+
+  let bounds = $derived.by(() => {
+		if (routeState) {
+			//if bounding box has 6 elements, it means it is a 3D bounding box, so we need to remove the last 2 elements
+			let EXTEND_FACTOR = 0.1;
+			let [minX, minY, maxX, maxY] =
+				routeState.bbox.length === 6 ? routeState.bbox.slice(0, 4) : routeState.bbox;
+
+			// Extend the bounding box by the given factor
+			const width = maxX - minX;
+			const height = maxY - minY;
+
+			minX -= width * EXTEND_FACTOR;
+			maxX += width * EXTEND_FACTOR;
+			minY -= height * EXTEND_FACTOR;
+			maxY += height * EXTEND_FACTOR;
+
+			return [minX, minY, maxX, maxY] as [number, number, number, number];
+		}
+	});
+</script>
+
+<MapLibre
+	center={[50, 20]}
+	zoom={7}
+	class="map"
+	{pitch}
+	{bounds}
+	standardControls
+	style={mapStyle}
+	images={[
+		{
+			id: 'arrow_sm',
+			url: './arrow_sm.png'
+		}
+	]}
+>
+	<MapLibreRoute route={routeState} {photoSelection} {routePoint} />
+</MapLibre>
