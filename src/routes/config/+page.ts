0d9c8d9d72a98bf72bc78b75dffde06434c7e9dd
Augustin Chassine <augustin.chassine@mabsilico.com>
2025-06-14 15:03:28 +0200

    fix: fix the build (indexedDB is not accessible when pre-rendering)

--- src/routes/config/+page.ts
+++ src/routes/config/+page.ts
@@ -1,10 +1,17 @@
 import type { PageLoad } from './$types';
-import { DEFAULT_EXPORT_OPTIONS } from '$lib/db_data/config.datatypes';
+import { DEFAULT_EXPORT_OPTIONS, type AppConfiguration } from '$lib/db_data/config.datatypes';
+import { browser } from '$app/environment';
 import { getCurrentAppConfig } from '$lib/db_data/localDB';
 
 export const load: PageLoad = async () => {
-	const config = await getCurrentAppConfig();
-	
+	let config: AppConfiguration;
+    // If running in the browser, fetch the current app config from indexedDB
+	if (browser) {
+		config = await getCurrentAppConfig();
+	} else {
+    // else, when pre-rendering on the server, use the default config
+		config = { exportOptions: DEFAULT_EXPORT_OPTIONS };
+	}
 	return {
 		defaultExportOptions: DEFAULT_EXPORT_OPTIONS,
 		config
